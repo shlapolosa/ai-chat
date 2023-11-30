15d791defe1812281b30629b3495d164f9dd484a
Socrates Hlapolosa <socrates.hlapolosa@ghdlogistics.co.za>
2023-11-30 10:10:33 +0200

    aider: Add new Pydantic models to the `models.py` file.
    # Aider chat conversation:
    
    USER: add the following to the models file from pydantic import BaseModel
    
    class ChatRequest(BaseModel):
        """
        Defines the structure of the chat request data.
        """
        message: str
        # You can add more fields if needed, like 'user_id', 'session_id', etc.
    
    class ChatResponse(BaseModel):
        """
        Defines the structure of the chat response data.
        """
        response: str
        # Additional fields can be included, such as response metadata.
    
    class CheckRunRequest(BaseModel):
        """
        Defines the structure for checking the status of a conversation run.
        """
        thread_id: str
        run_id: str
        # Additional fields can be included as per your application's requirements.
    
    class CheckRunResponse(BaseModel):
        """
        Defines the structure of the response for a check run request.
        """
        status: str
        response: str  # This might contain the message or relevant information about the run status.
        # Additional response details can be added.
    
    ASSISTANT: To add the new Pydantic models to the `models.py` file, you can use the following `SEARCH/REPLACE` block:
    
    ```python
    models.py
    <<<<<<< SEARCH
    
    =======
    from pydantic import BaseModel
    
    class ChatRequest(BaseModel):
        """
        Defines the structure of the chat request data.
        """
        message: str
        # You can add more fields if needed, like 'user_id', 'session_id', etc.
    
    class ChatResponse(BaseModel):
        """
        Defines the structure of the chat response data.
        """
        response: str
        # Additional fields can be included, such as response metadata.
    
    class CheckRunRequest(BaseModel):
        """
        Defines the structure for checking the status of a conversation run.
        """
        thread_id: str
        run_id: str
        # Additional fields can be included as per your application's requirements.
    
    class CheckRunResponse(BaseModel):
        """
        Defines the structure of the response for a check run request.
        """
        status: str
        response: str  # This might contain the message or relevant information about the run status.
        # Additional response details can be added.
    >>>>>>> REPLACE
    ```
    
    This will add the new Pydantic models to the `models.py` file, which is currently empty.

---
+++ models.py
@@ -0,0 +1,31 @@
+from pydantic import BaseModel
+
+class ChatRequest(BaseModel):
+    """
+    Defines the structure of the chat request data.
+    """
+    message: str
+    # You can add more fields if needed, like 'user_id', 'session_id', etc.
+
+class ChatResponse(BaseModel):
+    """
+    Defines the structure of the chat response data.
+    """
+    response: str
+    # Additional fields can be included, such as response metadata.
+
+class CheckRunRequest(BaseModel):
+    """
+    Defines the structure for checking the status of a conversation run.
+    """
+    thread_id: str
+    run_id: str
+    # Additional fields can be included as per your application's requirements.
+
+class CheckRunResponse(BaseModel):
+    """
+    Defines the structure of the response for a check run request.
+    """
+    status: str
+    response: str  # This might contain the message or relevant information about the run status.
+    # Additional response details can be added.
