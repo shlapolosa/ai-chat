0e3e57d263cfb5364339f7710c78944ba09c3660
Socrates Hlapolosa <socrates.hlapolosa@ghdlogistics.co.za>
2023-11-30 21:09:37 +0200

    changes

--- gpt_tools.py
+++ gpt_tools.py
@@ -1,5 +1,8 @@
 # This is a template and should be modified according to your application's needs.
 
+from config import settings
+
+
 def custom_response_formatter(response):
     """
     Formats the response from the OpenAI assistant in a custom way.
@@ -14,4 +17,34 @@ def data_preprocessor(input_data):
     # Implement your preprocessing logic here
     return processed_data
 
-# Add more utility functions or classes as needed for your application.
+
+# Add thread to DB with platform identifier
+def add_thread(thread_id, platform):
+    url = "https://api.airtable.com/v0/app9KJE69rZ0WLnpV/Threads" # replace this with your Airtable Web API URL
+    headers = {
+        "Authorization": settings.airtable_api_key,
+        "Content-Type": "application/json"
+    }
+    data = {
+        "records": [{
+            "fields": {
+                "Thread ID": thread_id,
+                "Platform": platform
+            }
+        }]
+    }
+
+    try:
+        response = requests.post(url, headers=headers, json=data)
+
+        if response.status_code == 200:
+            print("Thread added to DB successfully.")
+        else:
+        # Handle non-200 HTTP response status codes
+            print(
+                f"Failed to add thread: HTTP Status Code {response.status_code}, Response: {response.text}"
+            )
+    except Exception as e:
+        # Handle exceptions like network errors, request timeouts, etc.
+        print(f"An error occurred while adding the thread: {e}")
+
--- prompts.py
+++ prompts.py
@@ -3,13 +3,20 @@
 assistants = {
     "financial_advisor": {
         "active": True,
-        "assistant_id": "asst_F1kRl9Gd58ST0dVkjL4rE70W",
+        "assistant_id": "",
         "prompt": [
-            "Provide financial advice based on current market trends.",
-            "Answer queries related to investments and savings."
+            """
+    The assistant has been programmed to help people who are interested in Liam Ottley's AAA Accelerator program to learn about what it offers them as a paid member,
+    
+    A document has been provided with information on the Accelerator Program that should be used for all queries related to the Accelerator. If the user asks questions not related to what is included in the document, the assistant should say that they are not able to answer those questions. The user is chatting to the assistant on Instagram, so the responses should be kept brief and concise, sending a dense message suitable for instant messaging via Instagram DMs. Long lists and outputs should be avoided in favor of brief responses with minimal spacing. Also, markdown formatting should not be used. The response should be plain text and suitable for Instagram DMs.
+
+    The assistant has been programmed to never mention the knowledge "document" used for answers in any responses. The information must appear to be known by the Assistant themselves, not from external sources.
+
+    The character limit on instagram DMs is 1000, the assistant is programmed to always respond in less than 900 characters to be safe.
+"""
         ],
-        "tools": ["finance_tool"],
-        "knowledge_files": ["finance_knowledge.json"]
+        "tools": ["add_thread"],
+        "knowledge_files": ["knowledge/knowledge.docx"]
     },
     "general_assistant": {
         "active": False,
