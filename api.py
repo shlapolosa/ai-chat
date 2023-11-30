c12808c5e6b3e05bea63f85f00bdf1240243bc8b
Socrates Hlapolosa <socrates.hlapolosa@ghdlogistics.co.za>
2023-11-30 16:55:38 +0200

    Refactor root endpoint to return DetailsResponse model.

--- api.py
+++ api.py
@@ -2,10 +2,8 @@ from fastapi import APIRouter, Depends, HTTPException, Body, Response
 from main import logger
 from chat_service import ChatService
 from models import ChatRequest, ChatResponse, DetailsResponse
-from openai_assistant_manager import OpenAIAssistantManager
 from config import settings
 import os
-import re
 
 router = APIRouter()
 
@@ -21,7 +19,7 @@ def mask_sensitive_info(env_vars):
 
 router = APIRouter()
 
-@router.get("/", response_model=None)
+@router.get("/", response_model=DetailsResponse)
 async def read_root(chat_service: ChatService = Depends()) -> DetailsResponse:
     logger.info("Root endpoint was called")
     loaded_assistants = chat_service.get_loaded_assistants_info()
