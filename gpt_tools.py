9c29bd92644a3b67be1523e13eb6391bfc29983c
Socrates Hlapolosa <socrates.hlapolosa@ghdlogistics.co.za>
2023-11-30 12:00:37 +0200

    aider: Created prompts.py and gpt_tools.py with initial content.
    # Aider chat conversation:
    
    USER: create a files called prompts.py and gpt_tools.py in the current directory
    
    ASSISTANT: To create the new files `prompts.py` and `gpt_tools.py`, we will use the following *SEARCH/REPLACE blocks*:
    
    prompts.py
    <source>python
    <<<<<<< SEARCH
    =======
    # prompts.py
    # This file can contain mappings of actions to prompts for the OpenAI Assistant.
    
    assistants = {
        # Example assistant configuration
        "general_assistant": {
            "active": True,
            "prompt": "You are a helpful assistant.",
            "tools": "",
            "knowledge": ""
        },
        # Define additional assistants as needed
    }
    >>>>>>> REPLACE
    </source>
    
    gpt_tools.py
    <source>python
    <<<<<<< SEARCH
    =======
    # gpt_tools.py
    # This file can contain various utility functions to be used with the OpenAI Assistant.
    
    # Example utility function
    def example_tool():
        # Implement the tool functionality here
        pass
    
    # Add more utility functions as needed
    >>>>>>> REPLACE
    </source>
    
    These blocks define the initial content for the new files. You can add the actual implementation of the assistants' configurations and utility functions as required by your application.

---
+++ gpt_tools.py
@@ -0,0 +1,8 @@
+# This file can contain various utility functions to be used with the OpenAI Assistant.
+
+# Example utility function
+def example_tool():
+    # Implement the tool functionality here
+    pass
+
+# Add more utility functions as needed
---
+++ prompts.py
@@ -0,0 +1,12 @@
+# This file can contain mappings of actions to prompts for the OpenAI Assistant.
+
+assistants = {
+    # Example assistant configuration
+    "general_assistant": {
+        "active": True,
+        "prompt": "You are a helpful assistant.",
+        "tools": "",
+        "knowledge": ""
+    },
+    # Define additional assistants as needed
+}
