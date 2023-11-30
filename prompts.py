a9ff68451e741058248097d9db3b2091c1eed0c7
Socrates Hlapolosa <socrates.hlapolosa@ghdlogistics.co.za>
2023-11-30 12:02:12 +0200

    aider: Updated content of `prompts.py` file with assistant configurations for financial advisor, general assistant, and travel advisor.
    # Aider chat conversation:
    
    USER: update content of `prompts.py` file with the following code, dont leave any of the code out # This is a template and should be customized based on your application's needs.
    
    assistants = {
        "financial_advisor": {
            "active": True,
            "prompt": [
                "Provide financial advice based on current market trends.",
                "Answer queries related to investments and savings."
            ],
            "tools": ["finance_tool"],  # Example tool names, replace with actual tools.
            "knowledge_files": ["finance_knowledge.json"]  # Path to knowledge files.
        },
        "general_assistant": {
            "active": True,
            "prompt": [
                "Engage in general conversation.",
                "Provide information on a wide range of topics."
            ],
            "tools": ["general_knowledge_tool"],
            "knowledge_files": ["general_knowledge.json"]
        },
        "travel_advisor": {
            "active": False,  # This assistant is not active.
            "prompt": [
                "Suggest travel destinations.",
                "Provide travel tips and advisories."
            ],
            "tools": ["travel_tool"],
            "knowledge_files": ["travel_knowledge.json"]
        }
        # Add more assistant configurations as needed.
    }
    
    ASSISTANT: To update the content of the `prompts.py` file with the provided code, we will use the following *SEARCH/REPLACE block*:
    
    prompts.py
    <source>python
    <<<<<<< SEARCH
    # This file can contain mappings of actions to prompts for the OpenAI Assistant.
    
    assistants = {
        # Example assistant configuration
        "general_assistant": {
            "active": True,
            "prompt": "You are a helpful assistant.",
            "tools": "",
            "knowledge": ""
        },
        # Define additional assistants as needed
    }
    =======
    # This is a template and should be customized based on your application's needs.
    
    assistants = {
        "financial_advisor": {
            "active": True,
            "prompt": [
                "Provide financial advice based on current market trends.",
                "Answer queries related to investments and savings."
            ],
            "tools": ["finance_tool"],  # Example tool names, replace with actual tools.
            "knowledge_files": ["finance_knowledge.json"]  # Path to knowledge files.
        },
        "general_assistant": {
            "active": True,
            "prompt": [
                "Engage in general conversation.",
                "Provide information on a wide range of topics."
            ],
            "tools": ["general_knowledge_tool"],
            "knowledge_files": ["general_knowledge.json"]
        },
        "travel_advisor": {
            "active": False,  # This assistant is not active.
            "prompt": [
                "Suggest travel destinations.",
                "Provide travel tips and advisories."
            ],
            "tools": ["travel_tool"],
            "knowledge_files": ["travel_knowledge.json"]
        }
        # Add more assistant configurations as needed.
    }
    >>>>>>> REPLACE
    </source>
    
    This block will replace the entire content of the `prompts.py` file with the new code provided.

--- prompts.py
+++ prompts.py
@@ -1,12 +1,32 @@
-# This file can contain mappings of actions to prompts for the OpenAI Assistant.
+# This is a template and should be customized based on your application's needs.
 
 assistants = {
-    # Example assistant configuration
+    "financial_advisor": {
+        "active": True,
+        "prompt": [
+            "Provide financial advice based on current market trends.",
+            "Answer queries related to investments and savings."
+        ],
+        "tools": ["finance_tool"],  # Example tool names, replace with actual tools.
+        "knowledge_files": ["finance_knowledge.json"]  # Path to knowledge files.
+    },
     "general_assistant": {
         "active": True,
-        "prompt": "You are a helpful assistant.",
-        "tools": "",
-        "knowledge": ""
+        "prompt": [
+            "Engage in general conversation.",
+            "Provide information on a wide range of topics."
+        ],
+        "tools": ["general_knowledge_tool"],
+        "knowledge_files": ["general_knowledge.json"]
     },
-    # Define additional assistants as needed
+    "travel_advisor": {
+        "active": False,  # This assistant is not active.
+        "prompt": [
+            "Suggest travel destinations.",
+            "Provide travel tips and advisories."
+        ],
+        "tools": ["travel_tool"],
+        "knowledge_files": ["travel_knowledge.json"]
+    }
+    # Add more assistant configurations as needed.
 }
