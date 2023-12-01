2012e9dce5b31f20d374ef43c76dcc469c932cf7
Socrates Hlapolosa <socrates.hlapolosa@ghdlogistics.co.za>
2023-12-01 11:49:10 +0200

    aider: Changed references of `assistant_manager` to `_assistant_manager` inside `chat_service.py`.
    # Aider chat conversation:
    
    USER: should all the references of `assistant_manager` inside of the `chat_service.py` not be _assistant_manager?
    
    ASSISTANT: Yes, you are correct. The references to `assistant_manager` inside the `chat_service.py` file should be `_assistant_manager` to correctly access the instance variable. Let's make the necessary changes.
    
    Here are the *SEARCH/REPLACE* blocks:
    
    chat_service.py
    ```python
    <<<<<<< SEARCH
            assistant_id = self.assistant_manager.get_assistant_id_by_action(action)
            run_id = self.assistant_manager.send_message(thread_id, assistant_id, user_input)
    =======
            assistant_id = self._assistant_manager.get_assistant_id_by_action(action)
            run_id = self._assistant_manager.send_message(thread_id, assistant_id, user_input)
    >>>>>>> REPLACE
    ```
    
    chat_service.py
    ```python
    <<<<<<< SEARCH
                status = self.assistant_manager.check_run_status(thread_id, run_id)
    =======
                status = self._assistant_manager.check_run_status(thread_id, run_id)
    >>>>>>> REPLACE
    ```

--- chat_service.py
+++ chat_service.py
@@ -24,14 +24,14 @@ class ChatService:
         return thread_id
 
     async def handle_chat(self, action: str, thread_id: str, user_input: str):
-        assistant_id = self.assistant_manager.get_assistant_id_by_action(action)
-        run_id = self.assistant_manager.send_message(thread_id, assistant_id, user_input)
+        assistant_id = self._assistant_manager.get_assistant_id_by_action(action)
+        run_id = self._assistant_manager.send_message(thread_id, assistant_id, user_input)
         return run_id
 
     async def check_run_status(self, thread_id: str, run_id: str):
         start_time = time.time()
         while time.time() - start_time < 8:
-            status = self.assistant_manager.check_run_status(thread_id, run_id)
+            status = self._assistant_manager.check_run_status(thread_id, run_id)
             if status == 'completed':
                 # Assuming there's a method to fetch and clean the message
                 message_content = self._fetch_and_clean_message(thread_id)
