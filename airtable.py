6a94efed5cf5beb478ead36e1df508c8cf898181
Socrates Hlapolosa <socrates.hlapolosa@ghdlogistics.co.za>
2023-12-07 09:17:52 +0200

    aider: Change `log_to_airtable` function to read `thread_id` directly from `request_data.get('thread_id')` and update `log_endpoint` accordingly.
    # Aider chat conversation:
    
    USER: change `log_to_airtable` function, instead of recieving the thread_id as a parameter, read it directly from the request_data.get('thread_id') variable and update `log_endpoint` accordingly
    
    ASSISTANT: To make the requested changes, we need to modify the `log_to_airtable` function in `airtable.py` to read `thread_id` directly from `request_data.get('thread_id')` instead of receiving it as a parameter. We also need to update the `log_endpoint` decorator function to not pass `thread_id` explicitly to `log_to_airtable`.
    
    Here are the *SEARCH/REPLACE* blocks for the required changes:
    
    airtable.py
    ```python
    <<<<<<< SEARCH
    async def log_to_airtable(request_data, response_data, thread_id=None):
    =======
    async def log_to_airtable(request_data, response_data):
        thread_id = request_data.get('thread_id', "N/A")
    >>>>>>> REPLACE
    ```
    
    And for the `log_endpoint` function:
    
    airtable.py
    ```python
    <<<<<<< SEARCH
            await log_to_airtable(request_data, response_data, thread_id=request_data.get('thread_id'))
    =======
            await log_to_airtable(request_data, response_data)
    >>>>>>> REPLACE
    ```
    
    These changes will ensure that `thread_id` is handled within the `log_to_airtable` function, simplifying the interface and making the code more maintainable.

--- airtable.py
+++ airtable.py
@@ -32,7 +32,7 @@ def log_endpoint(func):
         print(f"Logging Response: {response_data}")
 
         # Pass thread_id to log_to_airtable if it exists in request_data
-        await log_to_airtable(request_data, response_data, thread_id=request_data.get('thread_id'))
+        await log_to_airtable(request_data, response_data)
 
         return response
     return wrapper
@@ -40,7 +40,8 @@ def log_endpoint(func):
 AIRTABLE_BASE_ID = "your_airtable_base_id"
 AIRTABLE_API_KEY = os.environ['AIRTABLE_API_KEY'] 
 
-async def log_to_airtable(request_data, response_data, thread_id=None):
+async def log_to_airtable(request_data, response_data):
+    thread_id = request_data.get('thread_id', "N/A")
     url = f"https://api.airtable.com/v0/{AIRTABLE_BASE_ID}/Requests"
     headers = {
         "Authorization": f"Bearer {AIRTABLE_API_KEY}",
