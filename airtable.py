dda937809ca7c719fa3446c5cbb49f3c2615978c
Socrates Hlapolosa <socrates.hlapolosa@ghdlogistics.co.za>
2023-12-07 15:52:49 +0200

    aider: Refactor code to handle parsing of response message from JSON or direct access.
    # Aider chat conversation:
    
    ASSISTANT: To implement the logic you've described, you would need to modify the code in `airtable.py` around line 89. The goal is to check if `response_data.get('body')` is a string and, if so, attempt to parse it as JSON to extract the `message` property. If it's not a string or if parsing fails, you would directly access the `message` property.
    
    Here's the step-by-step process to make this change:
    
    1. Check if `response_data.get('body')` is an instance of `str`.
    2. If it is a string, attempt to parse it as JSON.
    3. If parsing is successful, access the `message` property from the parsed JSON object.
    4. If parsing fails or if `response_data.get('body')` is not a string, access the `message` property directly.
    
    Here is the *SEARCH/REPLACE block* for the change:
    
    airtable.py
    ```python
    <<<<<<< SEARCH
                    "Response Message": response_data.get('body').get('message',"N/A"),
    =======
                    "Response Message": json.loads(response_data.get('body')).get('message') if isinstance(response_data.get('body'), str) else response_data.get('body', {}).get('message', "N/A"),
    >>>>>>> REPLACE
    ```
    
    This change uses a conditional expression to check the type of `response_data.get('body')`. If it's a string, it uses `json.loads` to parse it and then gets the `message`. If it's not a string, it assumes `response_data.get('body')` is a dictionary and tries to get the `message` from it, defaulting to `"N/A"` if not found.
    
    Please note that this code assumes that if `response_data.get('body')` is a string, it is in a valid JSON format. If the string might not be valid JSON, you should include error handling for `json.loads`. If you want to add error handling, please let me know, and I can provide the necessary code.

--- airtable.py
+++ airtable.py
@@ -86,7 +86,7 @@ async def log_to_airtable(request_data_serializable, response_data):
                 "Run ID": request_data_serializable.get('request', {}).get('run_id', "N/A"),
                 "Request Message": request_data_serializable.get('request', {}).get('message', "N/A"),  # Retrieve the message from request_data_serializable
                 "Action": request_data_serializable.get('action'),  # Include the Action field from request_data_serializable
-                "Response Message": response_data.get('body').get('message',"N/A"),
+                "Response Message": json.loads(response_data.get('body')).get('message') if isinstance(response_data.get('body'), str) else response_data.get('body', {}).get('message', "N/A"),
                 "Time": current_time  # Include the current time
             }
         }]
