4d5551792bf36a90a7dc2a2705c6fb921a0f0a59
Socrates Hlapolosa <socrates.hlapolosa@ghdlogistics.co.za>
2023-12-07 12:34:19 +0200

    Extract message from args or kwargs if present and add it to request_data.

--- airtable.py
+++ airtable.py
@@ -19,6 +19,12 @@ def log_endpoint(func):
             request_data['thread_id'] = kwargs['thread_id']
         print(f"Logging input parameters for action '{func.__name__}': {request_data}")
 
+        # Extract message from args or kwargs if present
+        message = next((arg for arg in args if isinstance(arg, str)), None)
+        if not message:
+            message = kwargs.get('message', kwargs.get('text', "N/A"))
+        request_data['message'] = message
+
         # Serialize request_data, handling UploadFile objects and other parameters
         def serialize_request_data(data):
             if isinstance(data, dict):
@@ -36,13 +42,7 @@ def log_endpoint(func):
             else:
                 return data
 
-        request_data_serializable = serialize_request_data(request_data)
-
-        # Extract message from args or kwargs if present
-        message = next((arg for arg in args if isinstance(arg, str)), None)
-        if not message:
-            message = kwargs.get('message', kwargs.get('text', "N/A"))
-        request_data_serializable['message'] = message
+        # request_data_serializable = serialize_request_data(request_data)
 
         # Call the actual endpoint function
         response = await func(*args, **kwargs)
@@ -55,7 +55,7 @@ def log_endpoint(func):
         print(f"Logging Response: {response_data}")
 
         # Pass thread_id and message to log_to_airtable if they exist in request_data
-        asyncio.create_task(log_to_airtable(request_data_serializable, response_data))
+        asyncio.create_task(log_to_airtable(request_data, response_data))
 
         return response
     return wrapper
@@ -66,6 +66,7 @@ async def log_to_airtable(request_data_serializable, response_data):
     AIRTABLE_API_KEY = os.environ['AIRTABLE_API_KEY'] 
 
     from datetime import datetime
+    print(f"log_to_airtable request_data: {request_data_serializable}")
     thread_id = request_data_serializable.get('thread_id', "N/A")
     current_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
     url = f"https://api.airtable.com/v0/{AIRTABLE_BASE_ID}/log"
